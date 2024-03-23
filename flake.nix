55c62960dd0d65046cb18d9c2aa35e36e5582f56
MOIS3Y <stepan@zhukovsky.me>
2024-03-23 17:10:44 +0900

    Update: flake.nix poetry2nix is now maintained out-of-tree nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,23 +2,42 @@
   description = "Script to suspend the xidlehook process and then start it again";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, poetry2nix, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+      inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+      inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryEnv;
     in {
-    packages.${system}.xidlehook-caffeine = with pkgs;
-      poetry2nix.mkPoetryApplication {
-        projectDir = self;
+
+    # Executed by `nix build .#<name>
+    # ? Example: nix build .#xidlehook-caffeine 
+    packages.${system} = {
+      xidlehook-caffeine = with pkgs; mkPoetryApplication { 
+        projectDir = self; 
         preferWheels = true;
         propagatedBuildInputs = [
           libnotify
           dunst
         ];
       };
+      default = self.packages.${system}.xidlehook-caffeine;
+    };
+
+    # Executed by `nix run . -- <args?>`
+    # ? example: nix run .#xidlehook-caffeine -- --version
+    apps.${system}.xidlehook-caffeine = {
+      type = "app";
+      program = "${self.packages.${system}.xidlehook-caffeine}/bin/xidlehook-caffeine";
+    };
+
     devShells.${system}.xidlehook-caffeine = pkgs.mkShellNoCC {
       shellHook = ''
         echo
@@ -28,7 +47,7 @@
         echo
       '';
       packages = with pkgs; [
-        (poetry2nix.mkPoetryEnv { projectDir = self; preferWheels = true; })
+        (mkPoetryEnv { projectDir = self; preferWheels = true; })
         libnotify
         dunst
       ];
